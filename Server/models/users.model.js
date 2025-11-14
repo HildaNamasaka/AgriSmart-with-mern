7d98b72f03c68892b2a4d11397ce8b65bebe539b
nasimiyunamasaka@gmail.com <nasimiyunamasaka@gmail.com>
2025-11-14 13:32:53 +0300

    client configuration

--- Server/models/users.model.js
+++ Server/models/users.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
 });
 
 //hashing the password before saving the user
-UserSchema,pre("save", async function(next) {
+userSchema.pre("save", async function(next) {
     if(!this.isModified("password")) return next();
     const salt = await bcrypt.genSalt(10);
     this.password =  await bcrypt.hash(this.password, salt);
--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from'dotenv';
 import connectDB from './config/db.js';
+import User from './models/users.model.js';
 
 
 dotenv.config();
@@ -10,3 +11,19 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.post('/users', async (req, res) => {
+    const user =  req.body;
+    if(!user.name || !user.phone || !user.email || !user.passworrd || !user.county){
+        return res.status(401).json({success: false, message: "please enter all fields"});
+    }
+
+    const newUser = new User(user);
+    try{
+        await newUser.save();
+        return res.status(201).json({success:true, daata: newUser});
+    }catch(error){
+        console.error('error in creating new product');
+        return res.status(500).json({success : failure, message:"internal server error"});
+    }
+    
+})
